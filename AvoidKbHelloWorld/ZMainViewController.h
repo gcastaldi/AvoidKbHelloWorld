bf2da572d979ad5ed2f0e80c07c3d757628291a7
Giannandrea Castaldi <g.castaldi@gmail.com>
2013-10-24 05:38:18 +0200

    added MainViewController

---
+++ AvoidKbHelloWorld/ZMainViewController.h
@@ -0,0 +1,13 @@
+//
+//  ZMainViewController.h
+//  AvoidKbHelloWorld
+//
+//  Created by Giannandrea Castaldi on 24/10/2013.
+//  Copyright (c) 2013 Giannandrea Castaldi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ZMainViewController : UIViewController
+
+@end
---
+++ AvoidKbHelloWorld/ZMainViewController.m
@@ -0,0 +1,38 @@
+//
+//  ZMainViewController.m
+//  AvoidKbHelloWorld
+//
+//  Created by Giannandrea Castaldi on 24/10/2013.
+//  Copyright (c) 2013 Giannandrea Castaldi. All rights reserved.
+//
+
+#import "ZMainViewController.h"
+
+@interface ZMainViewController ()
+
+@end
+
+@implementation ZMainViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
