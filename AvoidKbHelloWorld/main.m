181fe94ca86af9f698a8052be21252931e2c4b00
Giannandrea Castaldi <g.castaldi@gmail.com>
2013-10-24 05:22:11 +0200

    Initial Commit

---
+++ AvoidKbHelloWorld/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  AvoidKbHelloWorld
+//
+//  Created by Giannandrea Castaldi on 24/10/2013.
+//  Copyright (c) 2013 Giannandrea Castaldi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "ZAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+  @autoreleasepool {
+      return UIApplicationMain(argc, argv, nil, NSStringFromClass([ZAppDelegate class]));
+  }
+}
---
+++ AvoidKbHelloWorldTests/AvoidKbHelloWorldTests.m
@@ -0,0 +1,34 @@
+//
+//  AvoidKbHelloWorldTests.m
+//  AvoidKbHelloWorldTests
+//
+//  Created by Giannandrea Castaldi on 24/10/2013.
+//  Copyright (c) 2013 Giannandrea Castaldi. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface AvoidKbHelloWorldTests : XCTestCase
+
+@end
+
+@implementation AvoidKbHelloWorldTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
